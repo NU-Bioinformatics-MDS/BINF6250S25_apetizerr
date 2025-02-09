1c2121c75ef4067c8799d56463f3c1ab3b1ca9e8
mignardise <126769961+apetizerr@users.noreply.github.com>
2025-02-08 19:16:23 -0500

    project 5 beginning of smith waterman

---
+++ project05.py
@@ -0,0 +1,93 @@
+import numpy as np
+
+
+
+def cal_score(matrix, seq1, seq2, i, j, match, mismatch, gap):
+    '''Calculate score for position (i,j) in scoring matrix, also record move to trace back
+
+    Args:
+        matrix (numpy array): scoring matrix
+        seq1 (str): sequence 1
+        seq2 (str): sequence 2
+        i (int): current row number
+        j (int): current column number
+
+    Returns:
+        score in position (i,j)
+        move to trace back: 0-END, 1-DIAG, 2-UP, 3-LEFT
+
+    Pseudocode:
+        Calculate scores based on upper-left, up, and left neighbors:
+            diag_score = upper-left + (match or mismatch)
+            up_score = up + gap
+            left_score = left + gap
+        score = max(0, diag_score, up_score, left_score)
+        traceback = maximum direction or end
+
+    '''
+    pass
+
+
+def traceback(seq1, seq2, traceback_matrix, maximum_position):
+    '''Find the optimal path through scoring marix
+
+        diagonal: match/mismatch
+        up: gap in seq1
+        left: gap in seq2
+
+    Args:
+        seq1 (str) : First sequence being aligned
+        seq2 (str) : Second sequence being aligned
+        traceback_matrix (numpy array): traceback matrix
+        maximum_position (tuple): starting position to trace back from
+
+    Returns:
+        aligned_seq1 (str): e.g. GTTGAC
+        aligned_seq2 (str): e.g. GTT-AC
+
+    Pseudocode:
+        while current_move != END:
+            current_move = traceback_matrix[current_row][current_col]
+            if current_move == DIAG:
+                ...
+            elif current_move == UP:
+                ...
+            elif current_move == LEFT:
+                ...
+
+    '''
+    pass
+
+
+def smith_waterman(seq1, seq2, match=1, mismatch=-1, gap=-1):
+    '''Smith-Waterman algorithm for local alignment
+
+    Args:
+        seq1 (str): input seq 1
+        seq2 (str): input seq 2
+        match: default = +1
+        mismatch: default = -1
+        gap: default = -1
+
+    Returns:
+        aligned_seq1 (str)
+        aligned_seq2 (str)
+        score_matrix (numpy array): scoring matrix
+    '''
+
+    S = np.zeros((len(seq1)+1, len(seq2)+1)) # makes initial matrix for score only 0
+    T = np.zeros((len(seq1)+1, len(seq2)+1))
+
+    for i in range(S.shape[0]):
+        for j in range(T.shape[1]):
+            S[i][j] = max(S[i - 1][j - 1] + compute_diag_score, S[i - 1][j] + gap_score, H[i][j - 1] + gap_score, 0)
+            T[i][j] = direction of max(S[i - 1][j - 1] + compute_diag_score, S[i - 1][j] + gap_score, S[i][j - 1] + gap_score, 0)
+
+seq1 = 'TACTTAG'
+seq2 = 'CACATTAA'
+
+aligned_seq1, aligned_seq2, score_matrix = smith_waterman(seq1,seq2)
+
+print (aligned_seq1)
+print (aligned_seq2)
+print (score_matrix)
